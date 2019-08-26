ad1f03ad9d3de6a3d75b343a325bda72f2e63447
D14na Support <support@d14na.org>
2019-08-26 05:24:59 -0400

    Imported CoreUI (Vue).

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  lintOnSave: false,
+  runtimeCompiler: true,
+  publicPath: '',
+  baseUrl: '',
+  outputDir: undefined,
+  assetsDir: undefined,
+  productionSourceMap: false,
+  parallel: undefined,
+  css: undefined
+}
